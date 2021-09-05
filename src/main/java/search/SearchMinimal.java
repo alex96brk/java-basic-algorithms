b6eede87ee7901b1b57fe16aaf6b4649a65a646f
Alexey <alexey.bakulin96@gmail.com>
2021-09-05 17:09:42 +0300

    [add]: search folder. search minimal int realization

---
+++ src/main/java/search/SearchMinimal.java
@@ -0,0 +1,29 @@
+package search;
+
+import utils.BaseClass;
+
+public class SearchMinimal extends BaseClass {
+
+    public static void main(String[] args) {
+        int[] array1 = {2, 5, 3, 7, 4, 8, 6, 9, 2, 4, 3, 6, 4, 5, 7, 9, 8, 5, 3, 5};
+        printIntArray(array1);
+
+        int minValue = searchMinimal(array1);
+        System.out.println(String.format("Minimal value is %d", minValue));
+    }
+
+    public static int searchMinimal(int[] incomingArray) {
+        int valueMin = incomingArray[0];
+        int valueIndex = 0;
+
+        for (int i = 0; i < incomingArray.length; i++) {
+            if (incomingArray[i] < valueMin) {
+                valueMin = incomingArray[i];
+                valueIndex = i;
+            }
+        }
+
+        return valueMin;
+    }
+
+}
--- src/main/java/sort/SelectionSort.java
+++ src/main/java/sort/SelectionSort.java
@@ -8,4 +8,9 @@ public class SelectionSort extends BaseClass {
         int[] array1 = {2, 5, 3, 7, 4, 8, 6, 9, 0, 2, 4, 3, 6, 4, 5, 7, 9, 8, 0, 5, 3, 5};
         printIntArray(array1);
     }
+
+    public static void sortSelection_v1(int[] incomingIntArray) {
+
+    }
+
 }
