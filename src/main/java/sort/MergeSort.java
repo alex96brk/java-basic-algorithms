94fe4a20bfe1b5e8e24ff09fb178501993d025a4
Alexey <alexey.bakulin96@gmail.com>
2021-09-05 20:52:21 +0300

    [add]: add Merge Sort class wrapper

---
+++ src/main/java/sort/MergeSort.java
@@ -0,0 +1,14 @@
+package sort;
+
+import utils.BaseClass;
+
+public class MergeSort extends BaseClass {
+    public static void main(String[] args) {
+        int[] array1 = {2, 5, 3, 7, 4, 8, 6, 9, 0, 2, 4, 3, 6, 4, 5, 7, 9, 8, 0, 5, 3, 5};
+        System.out.println("Before sort:");
+        arrayToString(array1);
+
+        System.out.println("After sort:");
+    }
+
+}
