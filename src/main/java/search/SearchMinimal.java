1b1c352d0d55ef7843ab23977b8ba2355fd2875c
Alexey <alexey.bakulin96@gmail.com>
2021-09-05 19:30:59 +0300

    [add.edit]: selection sort implementation. edit search minimal

--- src/main/java/search/SearchMinimal.java
+++ src/main/java/search/SearchMinimal.java
@@ -13,22 +13,35 @@ public class SearchMinimal extends BaseClass {
         int[] array1 = {2, 5, 3, 7, 4, 8, 6, 9, 2, 4, 3, 6, 4, 5, 7, 9, 8, 5, 3, 5};
         printIntArray(array1);
 
-        int minValue = searchMinimal(array1);
+        int minValue = searchMinimalValue(array1, 0);
         System.out.println(String.format("Minimal value is %d", minValue));
+
+        int minValueIndex = searchMinimalValueIndex(array1, 0);
+        System.out.println(String.format("Minimal value index is %d", minValueIndex));
     }
 
-    public static int searchMinimal(int[] incomingArray) {
-        int valueMin = incomingArray[0];
-        int valueIndex = 0;
+    public static int searchMinimalValue(int[] incomingArray, int startIndex) {
+        int valueMin = incomingArray[startIndex];
 
         for (int i = 0; i < incomingArray.length; i++) {
             if (incomingArray[i] < valueMin) {
                 valueMin = incomingArray[i];
-                valueIndex = i;
             }
         }
-
         return valueMin;
+    }
+
+    public static int searchMinimalValueIndex(int[] incomingArray, int startIndex) {
+        int minimalValue = incomingArray[startIndex];
+        int minimalValueIndex = startIndex;
+
+        for (int i = 0; i < incomingArray.length; i++) {
+            if (incomingArray[i] < minimalValue) {
+                minimalValue = incomingArray[i];
+                minimalValueIndex = i;
+            }
+        }
+        return minimalValueIndex;
     }
 
 }
--- src/main/java/sort/SelectionSort.java
+++ src/main/java/sort/SelectionSort.java
@@ -12,10 +12,36 @@ public class SelectionSort extends BaseClass {
     public static void main(String[] args) {
         int[] array1 = {2, 5, 3, 7, 4, 8, 6, 9, 0, 2, 4, 3, 6, 4, 5, 7, 9, 8, 0, 5, 3, 5};
         printIntArray(array1);
+
+        sortSelection_v1(array1);
+        System.out.println("[SELECTION SORT]: Started");
+        printIntArray(array1);
     }
 
     public static void sortSelection_v1(int[] incomingIntArray) {
 
+        for (int step = 0; step < incomingIntArray.length; step++) {
+            int minValueIndex = searchMinimalValueIndex(incomingIntArray, step); //find minimal value index
+
+            int tempValue = incomingIntArray[step]; //save current value
+            incomingIntArray[step] = incomingIntArray[minValueIndex]; //swap current value and minimal value
+
+            incomingIntArray[minValueIndex] = tempValue;
+
+        }
+    }
+
+    public static int searchMinimalValueIndex(int[] incomingArray, int startIndex) {
+        int minimalValueIndex = startIndex;
+        int minimalValue = incomingArray[startIndex];
+
+        for (int i = startIndex + 1; i < incomingArray.length; i++) {
+            if (incomingArray[i] < minimalValue) {
+                minimalValue = incomingArray[i];
+                minimalValueIndex = i;
+            }
+        }
+        return minimalValueIndex;
     }
 
 }
