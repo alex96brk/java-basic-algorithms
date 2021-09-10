c2759ac7e7d7553e63c30589931c90cddae06b73
Alexey <alexey.bakulin96@gmail.com>
2021-09-10 18:04:32 +0300

    [edit]: Binary tree wrapper edit

--- src/main/java/trees/BinaryTree.java
+++ src/main/java/trees/BinaryTree.java
@@ -2,4 +2,13 @@ package trees;
 
 public class BinaryTree {
 
+    /**
+     * In this class i have been implemented
+     * Binary Tree Data Sort
+     *
+     * */
+    public static void main(String[] args) {
+
+    }
+
 }
