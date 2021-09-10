1a9a8c86f8d925a4149f37a4e0b396d9c80b0ba4
Alexey <alexey.bakulin96@gmail.com>
2021-09-10 19:55:20 +0300

    [edit]: count sort implementation added

--- src/main/java/sort/CountSort.java
+++ src/main/java/sort/CountSort.java
@@ -4,7 +4,8 @@ import utils.BaseClass;
 
 public class CountSort extends BaseClass {
     /**
-     * In this class i had implemented count sort of
+     * Сортировка подсчетом
+     * In this class i have been implemented count sort of
      * integer array values
      * */
 
@@ -12,5 +13,25 @@ public class CountSort extends BaseClass {
         int[] array1 = {2, 5, 3, 7, 4, 8, 6, 9, 0, 2, 4, 3, 6, 4, 5, 7, 9, 8, 0, 5, 3, 5};
         System.out.println("Before sort:");
         arrayToString(array1);
+
+        countSort(array1);
+        arrayToString(array1);
+    }
+
+    public static void countSort(int[] incomingArray) {
+        final int MAX_VALUE = 100;
+        int[] count = new int[MAX_VALUE];
+
+        for (int i = 0; i < incomingArray.length; i++) {
+            count[incomingArray[i]] = count[incomingArray[i]] + 1;
+        }
+
+        int arrIndex = 0;
+        for (int i = 0; i < count.length; i++) {
+            for (int j = 0; j < count[i]; j++) {
+                incomingArray[arrIndex] = i;
+                arrIndex++;
+            }
+        }
     }
 }
